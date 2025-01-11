480e5c8c0ca348691b816ad173f2824ffc57066b
Jalaj <83359199+Jalaj2002@users.noreply.github.com>
2025-01-12 04:50:36 +0530

    Add files via upload

---
+++ Events/script.js
@@ -0,0 +1,6 @@
+let button= document.getElementById("btn")
+
+button.addEventListener("click",()=>{
+    document.querySelector(".box").innerHTML="<b>Yayy you were clicked</b>"
+    //alert("I was clicked")
+})
